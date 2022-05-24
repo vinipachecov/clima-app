86a1b231d2387e790562aa097881397619a61ba1
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2022-05-24 16:31:37 -0300

    chore: add domain and data alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@data': './data',
+          '@domain': './domain',
+        },
+      },
+    ],
+  ],
 };
