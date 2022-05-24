2fec07b0e71308b00f4cf9ecd29a4d21926d4240
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2022-05-24 15:13:16 -0300

    base project commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+const App = () => (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    <Text>RN_CLIMA APP</Text>
+  </View>
+);
+export default App;
