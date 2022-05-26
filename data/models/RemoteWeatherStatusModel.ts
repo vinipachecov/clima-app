fb5bdfecc7dca4900186e56daa07e63f1eaad33e
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2022-05-26 12:40:24 -0300

    replace joi with yup validator

--- data/models/RemoteWeatherStatusModel.ts
+++ data/models/RemoteWeatherStatusModel.ts
@@ -1,18 +1,21 @@
-import joi from 'joi';
+import * as yup from 'yup';
 import { WeatherStatusEntity } from '@domain/entities/WeatherStatusEntity';
 import { UnexpectedError } from '@domain/errors/UnexpectedError';
 import { env } from '@main/config/sandbox.env';
 
-const RemoteWeatherStatusModelValidator = joi.object({
-  name: joi.string().required(),
-  weather: joi.array().items(
-    joi.object({
-      id: joi.number(),
-      main: joi.string().required(),
-      description: joi.string().required(),
-      icon: joi.string().required(),
+const RemoteWeatherStatusModelValidator = yup.object({
+  name: yup.string().required(),
+  weather: yup.array().of(
+    yup.object({
+      id: yup.number(),
+      main: yup.string().required(),
+      description: yup.string().required(),
+      icon: yup.string().required(),
     }),
   ),
+  main: yup.object({
+    temp: yup.number(),
+  }),
 });
 
 export type RemoteWeatherStatusModelProps = {
@@ -22,18 +25,20 @@ export type RemoteWeatherStatusModelProps = {
     description: string;
     icon: string;
   }[];
+  main: {
+    temp: number;
+  };
 };
 export class RemoteWeatherStatusModel {
-  static toEntity(model?: RemoteWeatherStatusModelProps) {
-    const isValid = RemoteWeatherStatusModelValidator.validate(model, {
-      allowUnknown: true,
-    });
-    if (!isValid.error) {
+  static async toEntity(model?: RemoteWeatherStatusModelProps) {
+    const isValid = await RemoteWeatherStatusModelValidator.isValidSync(model);
+    if (isValid) {
       return new WeatherStatusEntity({
         description: model!.weather[0].description,
         type: model!.weather[0].main,
         city: model!.name,
         icon: `${env.weatherImageAPI}${model!.weather[0].icon}@2x.png`,
+        temperature: model!.main.temp,
       });
     } else {
       throw new UnexpectedError();
