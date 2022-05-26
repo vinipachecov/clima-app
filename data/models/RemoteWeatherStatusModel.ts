b1b07ed64f08e4a71bc569194b51ed90c9c5bc1b
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2022-05-25 21:02:03 -0300

    Ensure RemoteWeatherStatusModel validates when mapping to entity

--- data/models/RemoteWeatherStatusModel.ts
+++ data/models/RemoteWeatherStatusModel.ts
@@ -1,14 +1,18 @@
 import joi from 'joi';
 import { WeatherStatusEntity } from '@domain/entities/WeatherStatusEntity';
 import { UnexpectedError } from '@domain/errors/UnexpectedError';
+import { env } from '@main/config/sandbox.env';
 
 const RemoteWeatherStatusModelValidator = joi.object({
   name: joi.string().required(),
-  weather: {
-    main: joi.string().required(),
-    description: joi.string().required(),
-    icon: joi.string().required(),
-  },
+  weather: joi.array().items(
+    joi.object({
+      id: joi.number(),
+      main: joi.string().required(),
+      description: joi.string().required(),
+      icon: joi.string().required(),
+    }),
+  ),
 });
 
 export type RemoteWeatherStatusModelProps = {
@@ -17,17 +21,19 @@ export type RemoteWeatherStatusModelProps = {
     main: string;
     description: string;
     icon: string;
-  };
+  }[];
 };
 export class RemoteWeatherStatusModel {
   static toEntity(model?: RemoteWeatherStatusModelProps) {
-    RemoteWeatherStatusModelValidator.validate(model);
-    if (RemoteWeatherStatusModelValidator.valid()) {
+    const isValid = RemoteWeatherStatusModelValidator.validate(model, {
+      allowUnknown: true,
+    });
+    if (!isValid.error) {
       return new WeatherStatusEntity({
-        description: model!.weather.description,
-        type: model!.weather.main,
+        description: model!.weather[0].description,
+        type: model!.weather[0].main,
         city: model!.name,
-        icon: model!.weather.icon,
+        icon: `${env.weatherImageAPI}${model!.weather[0].icon}@2x.png`,
       });
     } else {
       throw new UnexpectedError();
